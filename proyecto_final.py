08f9af6b44c99c1b9b555fa77f2ab12dcc8bcab9
EdurneSM <trad.tesm@gmail.com>
2024-05-20 15:03:36 +0200

    Versión 1.0.1

---
+++ proyecto_final.py
@@ -0,0 +1,13 @@
+################################################
+########## GESTOR DE TAREAS PENDIENTES #########
+################################################
+
+################################################
+"""TAREAS PERMITIDAS:
+- Agregar nueva tarea
+- Marcar una tarea como completada
+- Mostrar todas las tareas con su estado
+- Eliminar tareas
+"""
+################################################
+
