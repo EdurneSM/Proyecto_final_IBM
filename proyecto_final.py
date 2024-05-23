c69807abe675ba18f3888ac6f47f0b7e426065f7
EdurneSM <trad.tesm@gmail.com>
2024-05-23 13:42:05 +0200

    Version 1.0.3

--- proyecto_final.py
+++ proyecto_final.py
@@ -12,49 +12,107 @@
 ################################################
 
 
-# Declaración de una clase tarea, que contendrá datos sobre la posición en lista, nombre y estado de cada tarea
+# Creo una lista que imita a los elementos de la lista de tareas
+#  Contiene POSICIÓN - NOMBRE - ESTADO
+""" Tengo:
+- Tareas --> con posición, nombre y estado
+- Lista --> con las tareas
+- Menú --> con las opciones para el usuario"""
+
+###############################################################################################
+# Defino la clase tarea, con nombre y estado
 class Tarea:
-    def __init__(self, posicion, nombreTarea, is_completa = False):
-        self.posicion = posicion
+    def __init__(self, nombreTarea, is_completa = False):
         self.nombreTarea = nombreTarea
         self.is_completa = is_completa
+        self.posicion = None
     
     def __str__(self):
         estado = "Completada" if self.is_completa else "Pendiente" 
         return f"{self.posicion}. {self.nombreTarea} - {estado}"
 
+###############################################################################################
+# Defino la clase lista de tareas, que contendrá las tareas
+class ListaTareas:
+    def __init__(self):
+        self.tareas = []
+
+    def agregarTarea(self):
+        nombreTarea = input("Añade una nueva tarea a tu lista: ").strip()
+        nuevaTarea = Tarea(nombreTarea)
+        self.tareas.append(nuevaTarea)
+        self.actualizarPosicion()
+        print(f"La nueva tarea se ha añadido correctamente a tu lista")
+        # print("Has dejado el nombre de la tarea en blanco. Intenta escribir algo.")        Para la excepción
+        
+    def actualizarPosicion(self):
+        for i, tarea in enumerate(self.tareas, start = 1):
+            tarea.posicion = i
+
+    def mostrarTareas(self):
+        if not self.tareas:
+            print("No hay tareas en tu lista")
+        else:
+            for tarea in self.tareas:
+                print(tarea)
+    
+    def estadoCompletada(self, posicion):
+        if 0 < posicion <=len(self.tareas):
+            self.tareas[posicion -1].is_completa = True
+            print(f"Has marcado la tarea {posicion} como completada")
+        else:
+            print("El número de tarea que has introducido no es válido")
+    
+    def eliminarTareas(self, posicion):
+        if 0 < posicion <=len(self.tareas):
+            tareaEliminada = self.tareas.pop(posicion - 1)
+            self.actualizarPosicion()
+            print(f"Has eliminado la tarea '{tareaEliminada.nombreTarea}'")
+        else:
+            print("El número que has introducido no es un número de tarea correcto")
 
 
-
-############################################################################################################################
-# Declaración de la clase "Menú", que contiene todas las opciones que se mostrarán al usuario
+######################################################################################################
+# Clase con el menú
 class Menu:
     def __init__(self):
-        pass
+        self.listaTareas = ListaTareas()
         
     def mostrarMenu (self):
-        print("=======================================================\n"
-            "¡¡¡BIENVENIDO A TU LISTA DE TAREAS PERSONALIZABLE!!!\n"
-              "\nEstas son todas las opciones que tienes:\n"
-              "\n[A] Agregar una tarea nueva\n"
-              "[C] Marcar una tarea como completada\n"
-              "[E] Eliminar una tarea de la lista\n"
-              "[M] Mostrar todas las tareas de mi lista\n"
-              "[S] Salir del menú\n"
-              "=======================================================")
-        
-    def ejecutarOpcion (self):
-        opcionUsuario = input("¿Qué deseas hacer? Selecciona una opción: ")
-        if opcionUsuario == "A":
-            tarea = input("Escribe tu nueva tarea: ")
-        # elif opcionUsuario == "C":
-        #     tarea = 
-        elif opcionUsuario == "E":
-            # poner código de borrar elemento
-        elif opcionUsuario == "M":
-            print(lista)
-        else:
+        while True:
+            print("=======================================================\n"
+                "¡¡¡BIENVENIDO A TU LISTA DE TAREAS PERSONALIZABLE!!!\n"
+                "\nEstas son todas las opciones que tienes:\n"
+                "\n[A] Agregar una tarea nueva\n"
+                "[C] Marcar una tarea como completada\n"
+                "[E] Eliminar una tarea de la lista\n"
+                "[M] Mostrar todas las tareas de mi lista\n"
+                "[S] Salir del menú\n"
+                "=======================================================")
+            eleccion = input("Por favor, selecciona una opción: ").strip().upper()
             
-# Creado para probar si se imprime el menú - puede que tenga que borrarlo
-menu = Menu
-menu.mostrarMenu(0)
+            if eleccion == "A":
+                self.listaTareas.agregarTarea()
+            elif eleccion == "C":
+                try:
+                    pos = int(input("Indica el número de la tarea que quieres marcar como Completada: "))
+                    self.listaTareas.estadoCompletada(pos)
+                except ValueError:
+                    print("No hay ninguna tarea con ese número. Por favor, introduce un número de tarea válido")
+            elif eleccion == "E":
+                try:
+                    pos = int(input("Indica el número de la tarea que quieres eliminar de tu lista: "))
+                    self.listaTareas.eliminarTareas(pos)
+                except ValueError:
+                    print("No hay ninguna tarea con ese número. Por favor, introduce un número de tarea válido")
+            elif eleccion =="M":
+                self.listaTareas.mostrarTareas()
+            elif eleccion == "S":
+                print("¡Hasta la próxima!")
+                break
+            else:
+                print("La opción seleccionada no es válida. Por favor, indica qué quieres hacer")
+
+# Para probar las funciones 
+menu = Menu()
+menu.mostrarMenu()
